25ba81929f01f4859a6c2b938de49ec152a76cea
hongseongmun <101074859+hongseongmun@users.noreply.github.com>
2022-12-08 03:07:06 +0900

    오브젝트 풀링 수정

--- sb/14w.js
+++ sb/14w.js
@@ -1,33 +1,34 @@
 class Maps{
-    constructor(row,column){
-        this.Row = row;
-        this.Column = column;
-        this.mapArray = Array.from(Array(row), ()=> Array(column).fill(new Block((0))));
-
+    constructor(row, column){
+        this.mapArray = Array.from(Array(row), ()=> Array(column).fill(0));
+        for(var i =0; i<4; i++){
+            for(var j =0; j < 4; j++){
+                this.mapArray[i][j] = new Block(0);
+            }
+        }
         this.div_app = document.getElementById("app");        
     }
 
     addRandomPositionBlock(X, Y){
+       this.mapArray[X][Y].changevalue(2);
         this.mapArray[X][Y].setActive(true);
         this.mapArray[X][Y].setPosition(X,Y);
-        this.mapArray[X][Y].changevalue(2);
     
         this.div_app.appendChild(this.mapArray[X][Y].div_element);
     }
-
+//pop는 맨끝이니 오른쪽
+//shift는 맨 앞이니 왼쪽 
     moveright(){
         let moveMaps = this.mapArray;
         for(var i =0; i <4; i++){
             for(var j = 3; j >= 0; j--){
-               // console.log(this.mapArray[j][i]);
-                console.log(moveMaps[j][i]);
-                if(this.mapArray[j][i] == 0){
-                    this.mapArray[j][i] = moveMaps[j-1][i];
-                    console.log(this.mapArray[j][i]);
-                    this.mapArray[j][i].setMovePosition(j,i);
-                    this.div_app.appendChild(this.mapArray[j][i].div_element);
-
-                }
+                console.log(this.mapArray[j][i]);
+               // if(this.mapArray[j][i].Va == 0){
+                   // this.mapArray[j].pop();
+                  //  this.mapArray[j][i].Va = moveMaps[j-1][i].Va;
+                 //   this.mapArray[j][i].setMovePosition(j+1,i+1);
+                   // this.div_app.appendChild(this.mapArray[j][i].div_element);
+              //  }
             }
         }
     }
@@ -48,7 +49,7 @@ class Maps{
 
 class Block{
     constructor(value){
-        this.value = value;
+        this.Va = value;
         this.Active = false;
         this.XPos = 20;
         this.YPos = 20;
@@ -56,7 +57,7 @@ class Block{
         this.div_element.setAttribute("class","block");
     }
     changevalue(value){
-        this.value = value;
+        this.Va = value;
 
         var valuetext = document.createTextNode(value);
         var newtext = document.createTextNode(value);
@@ -78,6 +79,13 @@ class Block{
 
     }
 
+    setMovePosition(x,y){
+        this.XPos = 20 + 150 *x;
+        this.XPos = 20 + 150 *y;
+        this.div_element.style.left = this.XPos;
+        this.div_element.style.top = this.YPos;
+    }
+
 }
 
 var map = new Maps(4,4);
@@ -86,11 +94,12 @@ function randomblock(){
  var X = parseInt(Math.random()*4);
  var Y = parseInt(Math.random()*4);
 
-    if(map.mapArray[X][Y].value == 0){
+    if(map.mapArray[X][Y].Va == 0){
         map.addRandomPositionBlock(X, Y);
     }
-    else randomblock();
-
+    else  {
+        randomblock();
+    }
 
 }
 
